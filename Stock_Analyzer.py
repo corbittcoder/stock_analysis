cfef887084cd0f2b5202e4ef9b30848f8b714586
zackormond <zack_ormond@byu.edu>
2018-02-15 11:30:03 -0700

    Add Stubs

---
+++ Stock_Analyzer.py
@@ -0,0 +1,26 @@
+import json
+import requests
+
+
+
+
+
+class Stock_Analyzer():
+
+	def __init__(self):
+		self.twitter_key = ""
+		self.google_news_key = ""
+		self.google_finance_key = ""
+
+	def get_twitter_info(self):
+		pass
+
+	def get_google_finance(self):
+		pass
+
+	def get_google_news(self):
+		pass
+
+
+	def get_sentiment_analysis(self, string):
+		pass
