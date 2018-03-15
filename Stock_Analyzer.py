3ece787d73a5c2f1daa57b2ecb491abf638b018e
Scott A. Corbitt <scott.a.corbitt@gmail.com>
2018-03-15 12:11:19 -0600

    Not really sure what I did here, it was two weeks ago

--- Stock_Analyzer.py
+++ Stock_Analyzer.py
@@ -2,22 +2,87 @@ import boto3
 import json
 import requests
 import pandas as pd
+from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
 
 class Stock_Analyzer():
 
 	def __init__(self):
-		self.twitter_key = ""
-		self.google_news_key = ""
-		self.google_finance_key = ""
+		#self.twitter_key = ""
+		#self.google_news_key = ""
+		#self.google_finance_key = ""
+		pass
+
 
 	def get_twitter_info(self):
-		pass
+		ACCESS_TOKEN = '2546596742-n6nLv8UkmvgxdxK2bvHXhHlfq8XIOrNQl3utkBl'
+		ACCESS_SECRET = 'DEQjMDiqUR4qp5oie8SrmnEntCYUA1PtOP0RY0ofLoK9I'
+		CONSUMER_KEY = 'cBVRF9499iBTEXoB1Hcp3gLn5'
+		CONSUMER_SECRET = 'GfRVUV7cQvilsRacmdg7hpQikp7lcBqCZDN9OH5t9J4x3UEOyj'
+
+		oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+
+		# Initiate the connection to Twitter Streaming API
+		twitter_stream = TwitterStream(auth=oauth)
+
+		# Get a sample of the public data following through Twitter
+		iterator = twitter_stream.statuses.sample()
+
+		# Print each tweet in the stream to the screen
+		# Here we set it to stop after getting 1000 tweets.
+		# You don't have to set it to stop, but can continue running
+		# the Twitter API to collect data for days or even longer.
+		tweet_count = 1000
+		for tweet in iterator:
+			tweet_count -= 1
+			# Twitter Python Tool wraps the data returned by Twitter
+			# as a TwitterDictResponse object.
+			# We convert it back to the JSON format to print/score
+			print(json.dumps(tweet))
+
+			# The command below will do pretty printing for JSON data, try it out
+			# print json.dumps(tweet, indent=4)
+
+			if tweet_count <= 0:
+				break
 
 	def get_stock_info(self):
-		pass
+		api_key = 'ZXKLYB9LPX4Q4RXN'
+		symbol = 'MSFT'
+
+		url = ("https://www.alphavantage.co/query?"
+			   "function=TIME_SERIES_DAILY&"
+			   "symbol=" + symbol +
+			   "&apikey=" + api_key)
+
+		response = requests.get(url)
+		dictionary = response.json()
+
+
 
 	def get_stock_news(self):
-		pass
+		api_key = '4e9794825e034ddbb7439ae9b595ad31'
+
+		# select date
+		date = '2018-02-05'
+		# select company name
+		KeyWord = 'Microsoft'
+
+		url = ('https://newsapi.org/v2/everything?'
+				'q=' + KeyWord + '&'
+				'from=' + date + '&'
+				'sortBy=popularity&'
+				'apiKey=' + api_key)
+
+		response = requests.get(url)
+
+		# creates a python dictionary
+		dictionary = response.json()
+		print(type(dictionary))
+
+		# look through all popular articles that day and print the title and description
+		for i in range(len(dictionary['articles'])):
+			print(dictionary['articles'][i]['title'])
+			print(dictionary['articles'][i]['description'])
 
 
 	# Takes string to test sentiment
@@ -29,11 +94,11 @@ class Stock_Analyzer():
 		comprehend = session.client(service_name='comprehend', region_name='us-west-2')
 		sentiment_response = comprehend.detect_sentiment(Text = string, LanguageCode="en")
 		sentiment = sentiment_response["Sentiment"]
-		
+
 		# Get highest score from response
 		sentiment = sentiment[0] + sentiment[1:].lower()
 		sentiment_score = {sentiment: sentiment_response["SentimentScore"][sentiment]}
 		return sentiment_score
-	
+
 	def save_to_csv(self):
-		pass
+		pass #implement
--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from Stock_Analyzer import Stock_Analyzer
 
 analyzer = Stock_Analyzer()
 
-twitter_info = analyzer.get_twitter_info()
+#twitter_info = analyzer.get_twitter_info()
 stock_info = analyzer.get_stock_info()
 stock_news = analyzer.get_stock_news()
 analyzer.save_to_csv()
