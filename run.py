e221eab63d83707eb5e5a63e88658fc7bf045c08
zackormond <zack_ormond@byu.edu>
2018-03-15 12:24:03 -0600

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,8 @@ from Stock_Analyzer import Stock_Analyzer
 
 analyzer = Stock_Analyzer()
 
+print(analyzer.get_sentiment_analysis("Hello, today is a great day"))
 #twitter_info = analyzer.get_twitter_info()
-stock_info = analyzer.get_stock_info()
-stock_news = analyzer.get_stock_news()
-analyzer.save_to_csv()
+#stock_info = analyzer.get_stock_info()
+#stock_news = analyzer.get_stock_news()
+#analyzer.save_to_csv()
